87a2db7b8fdeb4458c1b86fe7f50e686ddbbe5b1
UsielOM <usiel31197@outlook.es>
2022-05-11 15:52:35 -0500

    Generando el metodo con pdfmaker

--- src/app/pdfmake/pdfmake.component.ts
+++ src/app/pdfmake/pdfmake.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { PdfMakeWrapper, Txt } from 'pdfmake-wrapper';
 
+import * as pdfMake from "pdfmake/build/pdfmake";
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+
+(<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
+
+
 @Component({
   selector: 'app-pdfmake',
   templateUrl: './pdfmake.component.html',
@@ -14,6 +20,18 @@ generarPDFWrapper(){
 
   );
   pdf.create().open()//Create para crear el pdf y open para que cuando se active la funcion se abra el pdf en una nueva pestaña
+}
+
+generarPDFMake(){
+  var dd = {
+	content: [
+		'First paragraph',
+		'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
+	]
+	
+}
+const pdf = pdfMake.createPdf(dd);
+pdf.open();
 }
   constructor() { }
 
