dae04d424305905c7fdba9c40da42963286db6d8
UsielOM <usiel31197@outlook.es>
2022-05-11 15:11:53 -0500

    Creando el componente pdfmake

--- src/app/pdfmake/pdfmake.module.ts
+++ src/app/pdfmake/pdfmake.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PDFMAKERoutingModule } from './pdfmake-routing.module';
+import { PdfmakeComponent } from './pdfmake.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PdfmakeComponent
+  ],
   imports: [
     CommonModule,
     PDFMAKERoutingModule
