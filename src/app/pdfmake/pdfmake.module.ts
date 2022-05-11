eb22f85302d0b5d35824dfa618229aa201e283e9
UsielOM <usiel31197@outlook.es>
2022-05-11 15:11:00 -0500

    Creando el modulo para el PDFMAKE

---
+++ src/app/pdfmake/pdfmake.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PDFMAKERoutingModule } from './pdfmake-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PDFMAKERoutingModule
+  ]
+})
+export class PDFMAKEModule { }
