640bf185fb365a6ed3b34b52bab9942c91331c89
UsielOM <usiel31197@outlook.es>
2022-05-11 15:26:03 -0500

    Creando la funcion para genear pdf y indicando el boton que funcion tiene que ejecutar

--- src/app/pdfmake/pdfmake.component.ts
+++ src/app/pdfmake/pdfmake.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PdfMakeWrapper, Txt } from 'pdfmake-wrapper';
 
 @Component({
   selector: 'app-pdfmake',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pdfmake.component.css']
 })
 export class PdfmakeComponent implements OnInit {
+generarPDFWrapper(){
+  const pdf = new PdfMakeWrapper(); //Creamos una nueva variable para poder usar los metodos del pdf Wrapper
+  pdf.add( //add para indicar que ñadiremos elemento nuevo
+    new Txt("Hellow World").bold().italics().end //Esta linea indica que agregaremos texto y su estilo end para indicar que se termino e agregar
 
+  );
+  pdf.create().open()//Create para crear el pdf y open para que cuando se active la funcion se abra el pdf en una nueva pestaña
+}
   constructor() { }
 
   ngOnInit(): void {
