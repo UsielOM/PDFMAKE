27ae55d8c42e16bab8ed0ea3ebadd2fab4dcb8ce
UsielOM <usiel31197@outlook.es>
2022-05-11 15:13:09 -0500

    Importando el pdfmake module al module principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PDFMAKEModule } from './pdfmake/pdfmake.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    PDFMAKEModule,
     AppRoutingModule
   ],
   providers: [],
