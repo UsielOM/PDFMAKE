64f4966fdfae89119a407e01e47258e9c15518a9
UsielOM <usiel31197@outlook.es>
2022-05-11 15:15:04 -0500

    Mandando a llamar al componente pdfmake para su visualizacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PdfmakeComponent } from './pdfmake/pdfmake.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component:PdfmakeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
